ac948745395fd9bcb07fa8b8fa225130398ada5b
Bryn Beaudry <babeaudry@gmail.com>
2017-09-21 14:40:36 -0700

    Added controller logic and shortened the views

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,20 +6,23 @@ class Welcome extends Application
 {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	* Index Page for this controller.
+	*
+	* Maps to the following URL
+	* 		http://example.com/
+	* 	- or -
+	* 		http://example.com/welcome/index
+	*
+	* So any other public methods not prefixed with an underscore will
+	* map to /welcome/<method_name>
+	* @see https://codeigniter.com/user_guide/general/urls.html
+	*/
 	public function index()
 	{
-		$this->load->view("welcome");
+		$this->data['pagebody'] = 'welcome';
+		$this->data['pageheader'] = 'Welcome!';
+		$this->data['pagetitle'] = 'Welcome!';
+		$this->render();
 	}
 
 }
